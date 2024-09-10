70312ae838f6015b24819e7d3a131f9aedfd12e3
Poddyman0 <127142059+Poddyman0@users.noreply.github.com>
2024-09-10 17:09:01 +0100

    changed routing

--- index.js
+++ index.js
@@ -2,31 +2,39 @@ const http = require("http");
 const url = require("url");
 const fs = require("fs");
 
-const page404 = fs.readFileSync("404.html", "utf-8", (err, data) => {
-  if (err) throw err;
-  return data;
+// Read the content of each HTML file
+const page404 = fs.readFileSync("404.html", "utf-8");
+const indexPage = fs.readFileSync("index.html", "utf-8");
+const aboutPage = fs.readFileSync("about.html", "utf-8");
+const contactMePage = fs.readFileSync("contact-me.html", "utf-8");
+
+// Create the server
+http.createServer(function (req, res) {
+  const q = url.parse(req.url, true);
+  let pageContent = "";
+  let statusCode = 200;
+  
+  // Route based on URL path
+  switch (q.pathname) {
+    case "/":
+      pageContent = indexPage;
+      break;
+    case "/about":
+      pageContent = aboutPage;
+      break;
+    case "/contact-me":
+      pageContent = contactMePage;
+      break;
+    default:
+      pageContent = page404;
+      statusCode = 404;
+      break;
+  }
+
+  // Send the response
+  res.writeHead(statusCode, { "Content-Type": "text/html" });
+  res.write(pageContent);
+  res.end();
+}).listen(8080, () => {
+  console.log("Server running at http://localhost:8080/");
 });
-
-http
-  .createServer(function (req, res) {
-    const q = url.parse(req.url, true);
-    let filename = "";
-    if (q.pathname === "/") {
-      filename = "." + "/index.html";
-    } else {
-      filename = "." + q.pathname;
-    }
-
-    fs.readFile(filename, function (err, data) {
-      if (err) {
-        res.writeHead(404, { "Content-Type": "text/html" });
-        res.write(page404);
-        return res.end();
-      } else {
-        res.writeHead(200, { "Content-Type": "text/html" });
-        res.write(data);
-        return res.end();
-      }
-    });
-  })
-  .listen(8080);
